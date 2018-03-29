e0704ced05037fdf2ef0b7b64307de7fd88550d3
Michael Gruenstaeudl <mi.gruenstaeudl@gmail.com>
2018-03-29 20:35:25 +0200

    2018.03.29.2000

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def my_test_suite():
 
 setup(
     name='annonex2embl',
-    version='0.4.3',
+    version='0.4.4',
     description='Converts annotated DNA sequence alignments in NEXUS format to ENA submission files',
     long_description=read('README.rst'),
     classifiers=[
