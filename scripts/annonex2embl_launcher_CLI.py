9e2d265f13052862841940489494fcc8d2402102
Michael Gruenstaeudl <mi.gruenstaeudl@gmail.com>
2019-10-16 20:23:22 +0200

    2019.10.16.2030

--- scripts/annonex2embl_launcher_CLI.py
+++ scripts/annonex2embl_launcher_CLI.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     
     import sys, os
     sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'annonex2embl'))
-    import CLIOps
+    from annonex2embl import CLIOps
 
     CLIOps.start_annonex2embl()
